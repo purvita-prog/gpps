67b40676179482d89692238e1d179b0c6046b62b
Purvita Gupta <purvita@adgonline.ca>
2026-03-13 12:54:07 +0000

    Home Page, Banner and navigation libraries, all internal pages like About, Resources, Careers etc.

--- web/themes/custom/gppolice/js/global.js
+++ web/themes/custom/gppolice/js/global.js
@@ -9,7 +9,11 @@
 
   Drupal.behaviors.bootstrap_barrio_subtheme = {
     attach: function (context, settings) {
-
+	document.querySelectorAll('.dropdown-toggle').forEach(function(el) {
+  el.addEventListener('click', function(e) {
+    window.location = this.href;
+  });
+});
     }
   };
 
